648d3f88b1f5d80398c6e254b1544eb11359d9c9
Mikel Cabezas <mikelcabezas@gmail.com>
2023-09-19 20:46:34 +0200

    start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialAngularModule } from './modules/material-angular/material-angular.module';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialAngularModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
